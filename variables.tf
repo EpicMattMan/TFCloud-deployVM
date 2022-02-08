e1bf6385620712f42da0a73da17f53f82b586bb3
EpicMattMan <66376412+EpicMattMan@users.noreply.github.com>
2022-02-08 10:46:57 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "rgName" {
+ type = string 
+}
+
+variable "location" {
+ type = string 
+}
+
+variable "subscription_id" {
+ type = string 
+}
+
+variable "client_id" {
+ type = string 
+}
+
+variable "clientSecret" {
+ type = string 
+}
+
+variable "tenant_id" {
+ type = string 
+}
